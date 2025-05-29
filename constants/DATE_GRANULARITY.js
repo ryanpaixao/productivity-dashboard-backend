a92a2e6177bff879a5d19b84b2fa69691ea30c14
Ryan Paixao <16196485+ryanpaixao@users.noreply.github.com>
2025-05-29 13:05:46 -0300

    Add DATE_GRANULARITY, ITEM_TYPES, PAGES constants

---
+++ constants/DATE_GRANULARITY.js
@@ -0,0 +1,24 @@
+const DATE_GRANULARITY = {
+  INTER_DAY: 'interday',
+  DAILY: 'daily',
+  WEEKLY: 'weekly',
+  MONTHLY: 'monthly',
+  YEARLY: 'yearly'
+};
+
+const TIME_UNIT = {
+  interday: 'day',
+  daily: 'day',
+  weekly: 'week',
+  monthly: 'month'
+};
+
+const NUM_OF_DAYS = {
+  interday: '1',
+  daily: '30',
+  weekly: '7',
+  monthly: '12',
+  yearly: '365'
+};
+
+export { DATE_GRANULARITY, NUM_OF_DAYS, TIME_UNIT };
